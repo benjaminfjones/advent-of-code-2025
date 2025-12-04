2dc841525c9308236c8f9835348442c29efdb5b4
Benjamin F Jones <benjaminfjones@gmail.com>
2025-12-03 19:41:21 -0800

    Add day 2, part 2

--- AdventOfCode2025/D2_2025.lean
+++ AdventOfCode2025/D2_2025.lean
@@ -1,3 +1,4 @@
+import Std
 import AdventOfCode2025.Basic
 
 
@@ -39,23 +40,23 @@ def parseInputStr : String → IntSet := fun input => List.map parseIntvl $ inpu
 
 def range (len: Nat) : List Int := List.map Int.ofNat (List.range len)
 
--- repeated numbers whose period is < 10^n. e.g. if n = 3, then 123123 is in the list
-def repeatedNums (n: Nat) : List Int :=
+-- numbers repeated 2x whose period is < 10^n. e.g. if n = 3, then 123123 is in the list
+def repeatedNums2 (n: Nat) : List Int :=
   let ds := range (10^n)
-  let ds' := ds.filter (fun d => d >= 10^(n-1))
+  let ds' := ds.filter (fun d => d >= 10^(n-1))  -- filter out numbers with leading zeros
   ds'.map (fun d => d*(10^n+1))
 
-#eval repeatedNums 2
-#eval List.length (repeatedNums 5)  -- 100,000
+#eval repeatedNums2 2
+#eval List.length (repeatedNums2 5)  -- 100,000
 
 /-- info: [] -/
 #guard_msgs in
 #eval let testSet := parseInputStr "1-10,50-100"
-      (repeatedNums 2).filter (fun x => intSetContains testSet x)
+      (repeatedNums2 2).filter (fun x => intSetContains testSet x)
 /-- info: [55, 66, 77, 88, 99] -/
 #guard_msgs in
 #eval let testSet := parseInputStr "1-10,50-100"
-      (repeatedNums 1).filter (fun x => intSetContains testSet x)
+      (repeatedNums2 1).filter (fun x => intSetContains testSet x)
 
 
 /------------------------------------------------------------------------
@@ -85,11 +86,78 @@ def parseInput (filepath: String) : IO IntSet := do
 #guard_msgs in
 #eval do
   let p1_test ← parseInput "data/d2_p1_test"
-  pure $ List.sum ((List.range 6).flatMap (fun n => (repeatedNums n).filter (fun x => intSetContains p1_test x)))
+  pure $ List.sum ((List.range 6).flatMap (fun n => (repeatedNums2 n).filter (fun x => intSetContains p1_test x)))
 
 /-- info: 52316131093 -/
 #guard_msgs in
 #eval do
   let p1_test ← parseInput "data/d2_p1"
-  pure $ List.sum ((List.range 6).flatMap (fun n => (repeatedNums n).filter (fun x => intSetContains p1_test x)))
+  pure $ List.sum ((List.range 6).flatMap (fun n => (repeatedNums2 n).filter (fun x => intSetContains p1_test x)))
 
+
+
+/------------------------------------------------------------------------
+ Part 2
+
+ "Now, an ID is invalid if it is made only of some sequence of digits repeated
+ at least twice. So, 12341234 (1234 two times), 123123123 (123 three times),
+ 1212121212 (12 five times), and 1111111 (1 seven times) are all invalid IDs."
+
+ -----------------------------------------------------------------------/
+
+-- numbers repeated N times whose period is < 10^n. e.g. if N=3 and n = 2, then 646464 = 64*(10000 + 100 + 1) is in the list
+def repeatedNumsN (N n: Nat) : List Int :=
+  let ds := range (10^n)
+  let ds' := ds.filter (fun d => d >= 10^(n-1))  -- filter out numbers with leading zeros
+  let multiplier := List.sum (List.map (fun e => 10^(e*n)) (List.range N))
+  ds'.map (fun d => d*multiplier)
+
+/- #eval repeatedNumsN 3 2 -/
+#guard (repeatedNumsN 3 2).length == 90
+#eval (repeatedNumsN 10 1)
+#guard (repeatedNums2 3).length == 900
+-- there are overlaps
+#guard
+  let set := Std.HashSet.emptyWithCapacity 1000
+  let set' := set.insertMany (repeatedNumsN 3 2)
+  let set'' := set'.insertMany (repeatedNums2 3)
+  let nums := set''.toList
+  nums.length == 981
+
+def overallRepeatedNums :=
+  let set := Std.HashSet.emptyWithCapacity 1000
+  -- numbers repeated twice w/ period < 10^n
+  let set0 := set.insertMany  $ (List.range 6).flatMap (fun n => (repeatedNums2 n))
+  -- numbers repeated twice w/ period < 10^n
+  let set1 := set0
+  /- let set1 := set0.insertMany $ (List.range 6).flatMap (fun n => (repeatedNumsN 2 n)) -/
+  -- numbers repeated 3x w/ period < 10^n
+  let set2 := set1.insertMany $ (List.range 5).flatMap (fun n => (repeatedNumsN 3 n))
+  -- TODO: make it a fold
+  let set3 := set2.insertMany $ (List.range 4).flatMap (fun n => (repeatedNumsN 4 n))
+  let set4 := set3.insertMany $ (List.range 3).flatMap (fun n => (repeatedNumsN 5 n))
+  let set5 := set4.insertMany $ (List.range 2).flatMap (fun n => (repeatedNumsN 6 n))
+  let set6 := set5.insertMany $ (List.range 2).flatMap (fun n => (repeatedNumsN 7 n))
+  let set7 := set6.insertMany $ (List.range 1).flatMap (fun n => (repeatedNumsN 8 n))
+  let set8 := set7.insertMany $ (List.range 1).flatMap (fun n => (repeatedNumsN 9 n))
+  let set9 := set8.insertMany $ (List.range 1).flatMap (fun n => (repeatedNumsN 10 n))
+  set9.toList
+
+#eval overallRepeatedNums.length
+
+/-- info: 4174379265 -/
+#guard_msgs in
+#eval do
+  let p2_test ← parseInput "data/d2_p1_test"
+  pure $ List.sum (overallRepeatedNums.filter (fun x => intSetContains p2_test x))
+
+/-- info: 69564213293 -/
+#guard_msgs in
+#eval do
+  /- let startTime ← BaseIO.toIO IO.monoMsNow -/
+  let p2 ← parseInput "data/d2_p1"
+  let result := List.sum (overallRepeatedNums.filter (fun x => intSetContains p2 x))
+  /- let endTime ← BaseIO.toIO IO.monoMsNow -/
+  /- let duration := endTime - startTime -/
+  /- IO.println s!"time: {duration}" -/
+  pure $ result
