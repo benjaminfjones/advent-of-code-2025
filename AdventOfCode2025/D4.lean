a8062e1f721ef9a61ba752692ac2b7d90b029c41
Benjamin F Jones <benjaminfjones@gmail.com>
2025-12-16 13:02:55 -0800

    Start a dependently typed 2d grid

--- AdventOfCode2025/D4.lean
+++ AdventOfCode2025/D4.lean
@@ -6,33 +6,32 @@ variable {α: Type} [Inhabited α]
 structure Grid α where
   data: Array (Array α)
   validRows: 0 < data.size
-  validCols: ∀ i, (h : 0 ≤ i ∧ i < data.size) → 0 < (data[i]'h.right).size
+  validCols: ∀ i, (h: i < data.size) → 0 < (data[i]'h).size
   nrows: Nat
   ncols: Nat
   nrowEqn: nrows = data.size
-  ncolEqn: ∀ i, (h : 0 ≤ i ∧ i < data.size) → ncols = (data[i]'h.right).size
+  ncolEqn: ∀ i, (h : i < data.size) → ncols = (data[i]'h).size
 
 def Grid.replicate (rows cols: Nat) (a: α) : Grid α :=
   sorry
 
-instance : GetElem (Grid α) (Nat × Nat) α (fun g rc => (0 ≤ rc.fst ∧ rc.fst < g.nrows) ∧ (0 ≤ rc.snd ∧ rc.snd < g.ncols)) where
-  getElem g rc valid := 
+instance : GetElem (Grid α) (Nat × Nat) α (fun g rc => rc.fst < g.nrows ∧ rc.snd < g.ncols) where
+  getElem g rc valid :=
     have hr : rc.fst < g.data.size := by
       rw [g.nrowEqn] at valid
-      exact valid.left.right
+      exact valid.left
     have hc : rc.snd < (g.data[rc.fst]'hr).size := by
       have : g.ncols = (g.data[rc.fst]'hr).size := by
-        refine g.ncolEqn rc.fst ⟨?_, ?_⟩
-        . exact valid.left.left
-        . rw [g.nrowEqn] at valid
-          exact valid.left.right
+        apply g.ncolEqn rc.fst
       rw [this] at valid
-      exact valid.right.right
+      exact valid.right
     (g.data[rc.fst]'hr)[rc.snd]'hc
 
-instance : GetElem? (Grid α) (Nat × Nat) α (fun g rc => (0 ≤ rc.fst ∧ rc.fst < g.nrows) ∧ (0 ≤ rc.snd ∧ rc.snd < g.ncols)) where
+instance : GetElem? (Grid α) (Nat × Nat) α (fun g rc => rc.fst < g.nrows ∧ rc.snd < g.ncols) where
   getElem? g rc := (g.data[rc.fst]?) >>= λ row => row[rc.snd]?
   getElem! g rc := g.data[rc.fst]![rc.snd]!
+
+def Grid.set (g: Grid α) (idx: Nat × Nat) (v: α) (h: idx.fst < g.nrows ∧ idx.snd < g.ncols) : Grid α := sorry
 
 end Grid
 
